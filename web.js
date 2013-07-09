33e3bf34b550ede98ec645aa52b1ddb16ba410b2
Nuno Pato <nuno.pato@lmit.pt>
2013-07-09 03:27:15 +0100

    Message read from buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = fs.readFileSync('index.html'); // It returns itself a buffer when no encoding is passed
+  response.send(file.toString());
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
